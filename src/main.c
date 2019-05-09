5f72c5bfc45d3086340e891e931e1701f691eebd
Kurausukun <lord.uber1@gmail.com>
2019-05-08 20:45:23 -0400

    fix RNG

--- src/main.c
+++ src/main.c
@@ -98,6 +98,7 @@ void AgbMain()
     CheckForFlashMemory();
     InitMainCallbacks();
     InitMapMusic();
+	SeedRngWithRtc();
     ClearDma3Requests();
     ResetBgs();
     SetDefaultFontsPointer();
