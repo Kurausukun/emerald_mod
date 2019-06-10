b77d09d4b55f3a6e87953d84cb7162d1a2065d7e
Kurausukun <lord.uber1@gmail.com>
2019-06-10 17:40:13 -0400

    add checks for pomeg berry

--- src/pokemon.c
+++ src/pokemon.c
@@ -2996,6 +2996,74 @@ void CalculateMonStats(struct Pokemon *mon)
     SetMonData(mon, MON_DATA_HP, &currentHP);
 }
 
+void CalculateMonStatsPomegSafety(struct Pokemon *mon)
+{
+    s32 oldMaxHP = GetMonData(mon, MON_DATA_MAX_HP, NULL);
+    s32 currentHP = GetMonData(mon, MON_DATA_HP, NULL);
+    s32 hpIV = GetMonData(mon, MON_DATA_HP_IV, NULL);
+    s32 hpEV = GetMonData(mon, MON_DATA_HP_EV, NULL);
+    s32 attackIV = GetMonData(mon, MON_DATA_ATK_IV, NULL);
+    s32 attackEV = GetMonData(mon, MON_DATA_ATK_EV, NULL);
+    s32 defenseIV = GetMonData(mon, MON_DATA_DEF_IV, NULL);
+    s32 defenseEV = GetMonData(mon, MON_DATA_DEF_EV, NULL);
+    s32 speedIV = GetMonData(mon, MON_DATA_SPEED_IV, NULL);
+    s32 speedEV = GetMonData(mon, MON_DATA_SPEED_EV, NULL);
+    s32 spAttackIV = GetMonData(mon, MON_DATA_SPATK_IV, NULL);
+    s32 spAttackEV = GetMonData(mon, MON_DATA_SPATK_EV, NULL);
+    s32 spDefenseIV = GetMonData(mon, MON_DATA_SPDEF_IV, NULL);
+    s32 spDefenseEV = GetMonData(mon, MON_DATA_SPDEF_EV, NULL);
+    u16 species = GetMonData(mon, MON_DATA_SPECIES, NULL);
+    s32 level = GetLevelFromMonExp(mon);
+    s32 newMaxHP;
+
+    SetMonData(mon, MON_DATA_LEVEL, &level);
+
+    if (species == SPECIES_SHEDINJA)
+    {
+        newMaxHP = 1;
+    }
+    else
+    {
+        s32 n = 2 * gBaseStats[species].baseHP + hpIV;
+        newMaxHP = (((n + hpEV / 4) * level) / 100) + level + 10;
+    }
+
+    gBattleScripting.field_23 = newMaxHP - oldMaxHP;
+    if (gBattleScripting.field_23 == 0)
+        gBattleScripting.field_23 = 1;
+
+    SetMonData(mon, MON_DATA_MAX_HP, &newMaxHP);
+
+    CALC_STAT(baseAttack, attackIV, attackEV, STAT_ATK, MON_DATA_ATK)
+    CALC_STAT(baseDefense, defenseIV, defenseEV, STAT_DEF, MON_DATA_DEF)
+    CALC_STAT(baseSpeed, speedIV, speedEV, STAT_SPEED, MON_DATA_SPEED)
+    CALC_STAT(baseSpAttack, spAttackIV, spAttackEV, STAT_SPATK, MON_DATA_SPATK)
+    CALC_STAT(baseSpDefense, spDefenseIV, spDefenseEV, STAT_SPDEF, MON_DATA_SPDEF)
+
+    if (species == SPECIES_SHEDINJA)
+    {
+        if (currentHP != 0 || oldMaxHP == 0)
+            currentHP = 1;
+        else
+            return;
+    }
+    else
+    {
+        if (currentHP == 0 && oldMaxHP == 0)
+            currentHP = newMaxHP;
+        else if (currentHP != 0)
+        {
+            currentHP += newMaxHP - oldMaxHP;
+            if (currentHP < 1)
+                currentHP = 1;
+        }
+        else
+            return;
+    }
+
+    SetMonData(mon, MON_DATA_HP, &currentHP);
+}
+
 void BoxMonToMon(const struct BoxPokemon *src, struct Pokemon *dest)
 {
     u32 value = 0;
@@ -5000,7 +5068,10 @@ bool8 PokemonUseItemEffects(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mov
                                 dataSigned = 0;
                         }
                         SetMonData(mon, sGetMonDataEVConstants[var_38], &dataSigned);
-                        CalculateMonStats(mon);
+                        if (NuzlockeFlagGet(GLOBAL_NUZLOCKE_SWITCH) == 1)
+                            CalculateMonStatsPomegSafety(mon);
+                        else
+                            CalculateMonStats(mon);
                         var_3C++;
                         retVal = FALSE;
                         break;
