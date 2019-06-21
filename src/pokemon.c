ca020f9863b14d694db490e4131d826ef1a673fa
Kurausukun <lord.uber1@gmail.com>
2019-06-20 23:21:14 -0400

    use new cymbal crash sample, make wally use rival theme

--- src/pokemon.c
+++ src/pokemon.c
@@ -6206,10 +6206,6 @@ u16 GetBattleBGM(void)
         case TRAINER_CLASS_CHAMPION:
             return MUS_BATTLE33;
         case TRAINER_CLASS_PKMN_TRAINER_3:
-            if (gBattleTypeFlags & BATTLE_TYPE_FRONTIER)
-                return MUS_BATTLE35;
-            if (!StringCompare(gTrainers[gTrainerBattleOpponent_A].trainerName, gText_BattleWallyName))
-                return MUS_BATTLE20;
             return MUS_BATTLE35;
         case TRAINER_CLASS_ELITE_FOUR:
             return MUS_BATTLE38;
